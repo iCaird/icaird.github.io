2a79ddf8afd0b7bdbbe7abe7c0ac1dc289b0a1c2
Ellis Caird <ellisc150600@gmail.com>
2025-11-16 11:29:34 +0000

    lots of calkin-wilf sketch improvements

--- public/sketches/calkin-wilf.js
+++ public/sketches/calkin-wilf.js
@@ -4,62 +4,123 @@ let circles = [];
 let leftButton;
 let rightButton;
 let resetButton;
+let started = false;
+let one;
+let tex;
+let TEXT_SIZE = 16;
 function setup() {
   pixelDensity(1);
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(window.innerWidth,window.innerHeight);
   background(220);
+  tex = createP();
+  tex.style("'font-size', '14px'");
+  tex.position(0,0);
+  katex.render('\\nabla^2{2}\\Phi=\\sigma(x)', tex.elt);
   leftButton = createButton('L');
   rightButton = createButton('R');
-  leftButton.position(10, height + -20);
-  rightButton.position(50, height + -20);
+  leftButton.position(10, height + -25);
+  rightButton.position(35, height + -25);
   leftButton.mousePressed(() => left());
   rightButton.mousePressed(() => right());
   resetButton = createButton('Reset');
-  resetButton.position(90, height + -20);
+  resetButton.position(62.5, height + -25);
   resetButton.mousePressed(() => {
-    nodes = [nodes[0]];
+    nodes = [{
+    x: width / 2,
+    val: [1, 1],
+    y : 10,
+    a: 255,
+    vel: 1,
+    acc: 0.03
+  }];
     circles = [];
+    started = false;
   });
   nodes.push({
     x: width / 2,
     val: [1, 1],
-    y : 10
-  })
+    y : 10,
+    a: 255,
+    vel: 1,
+    acc: 0.03
+  });
 }
 
+
+
 function draw() {
-  background(220);
+  background(15, 23, 42);
   translate(0, height/2);
   textAlign(CENTER, CENTER)
+  stroke(120)
   line(line_pad, -line_pad, width - line_pad, -line_pad)
+
   for (let c of circles) {
+    if (c.a <= 5) circles.splice(circles.indexOf(c), 1);
     push()
     noFill();
-    stroke(0, 0, 0, c.a);
-    strokeWeight(map(c.a, 0, 255, 1, 4))
+    colorMode(HSB)
+    stroke(map(c.a,255,0,0,360), 255, 100, c.a);
+    strokeWeight(map(c.a, 0, 255, 2, 6))
     // circle(c.x,-line_pad,c.d);
     arc(c.x, -line_pad, c.d, c.d, c.dir == -1 ? PI : 0, c.dir == -1 ? 0 : PI);
     pop();
-    if (c.a <= 0) circles.splice(circles.indexOf(c), 1);
   }
   for (let node of nodes) {
-    fill(0);
-    circle(node.x, -line_pad, 5)
-    text(`${node.val[0]}/${node.val[1]}`, node.x, node.y)
-    node.y += node.y > 0 ? 1 : -1;
+    if (node.a <= 5) nodes.splice(nodes.indexOf(node), 1);
+    push()
+    colorMode(HSB);
+    fill(map(node.a,255,0,0,360),100,255,node.a);
+    strokeWeight(0.1);
+    noStroke();
+    //circle(node.x, -line_pad, 5);
+    if (started) {
+
+    node.y += node.y > 0 ? node.vel : -node.vel;
+    node.vel += node.acc;
+    }
+    pop();
   }
+  push();
+  colorMode(HSB);
+  textAlign(CENTER, CENTER);
+    textSize(TEXT_SIZE);
+    textFont("monospace");
+    noStroke();
+  for (let node of nodes) {
+    let colr = map(node.a,255,0,0,360);
+    fill(colr,100,255,node.a);
+    // text(`${node.val[0]}/${node.val[1]}`, node.x, node.y);
+    drawFraction(node.val[0], node.val[1], node.x, node.y + 10,colr);
+  }
+  pop();
 
 
 }
 
+function drawFraction(num, den, x, y, c) {
+  text(num, x, y - 10);
+  push();
+  let numWidth = textWidth(num);
+  let denWidth = textWidth(den);
+  let maxWidth = max(numWidth, denWidth);
+  stroke(c,100,255);
+  line(x - maxWidth / 2, y, x + maxWidth / 2, y);
+  pop();
+  text(den, x, y + 10);
+}
 function left() {
   let lastnode = nodes.at(-1);
   let lastnum = lastnode.val[0];
   let lastden = lastnode.val[1];
- nodes.push({
-      x: map(lastnum / (lastnum + lastden), 0, 1, line_pad, width / 2),
-      val: [lastnum, 2 * lastnum + lastden],
-      y : 10
+  started = true;
+  nodes.push({
+      x: map(lastnum / (lastnum + lastden), 0, 1, 0, width / 2),
+      val: [lastnum, lastnum + lastden],
+      y : 10,
+      a: 255,
+      vel: 1,
+      acc: 0.03
     })
     let currnode = nodes.at(-1);
     let x = (lastnode.x + currnode.x) / 2
@@ -74,15 +135,22 @@ function left() {
     for (let c of circles) {
       c.a -= 10;
     } 
+    for (let n of nodes) {
+      n.a -= 10;
+    }
 }
 function right() {
+  started = true;
   let lastnode = nodes.at(-1);
   let lastnum = lastnode.val[0];
   let lastden = lastnode.val[1];
   nodes.push({
-      x: map(log((lastnum + lastden) / lastden), log(1), log(20), width / 2, width - line_pad),
+      x: map(lastden/(lastden+lastnum), 0, 1, width, width/2),
       val: [lastnum + lastden, lastden],
-      y : -10
+      y : -10,
+      a: 255,
+      vel: 1,
+      acc: 0.03
     })
     let currnode = nodes.at(-1);
     let x = (lastnode.x + currnode.x) / 2
@@ -96,6 +164,9 @@ function right() {
     for (let c of circles) {
       c.a -= 10;
     }
+    for (let n of nodes) {
+      n.a -= 10;
+    }
 }
 function keyPressed() {
   if (keyCode === LEFT_ARROW) {
@@ -103,7 +174,15 @@ left();
   } else if (keyCode === RIGHT_ARROW) {
     right();
   } else if (key === 'r') {
-    nodes = [nodes[0]];
+    started = false;
+    nodes = [{
+    x: width / 2,
+    val: [1, 1],
+    y : 10,
+    a: 255,
+    vel: 1,
+    acc: 0.03
+  }];
     circles = [];
   }
 }
