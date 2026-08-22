29bb67f2d5721e363ca8fb2e0f63953a0edde243
ecDesktop <ellisc150600@gmail.com>
2026-08-22 12:12:42 +0100

    commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Geist Sans", ...defaultTheme.fontFamily.sans],
+        sans: ["Roboto Slab", "Bitter", ...defaultTheme.fontFamily.sans],
         mono: ["Geist Mono", ...defaultTheme.fontFamily.mono],
       },
     },
